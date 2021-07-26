d6260b7d2911a7ac197bfef442b672f542655df6
Jefersson Nathan <malukenho.dev@gmail.com>
2021-07-26 23:55:09 +0200

    Initial code for auto-register xulieta components
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Register.php
+++ src/Register.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Codelicia\Xulieta\AutoPlugin;
 
 use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
 use Composer\Json\JsonFile;
@@ -16,8 +18,30 @@ use Composer\Package\RootPackageInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
-use JetBrains\PhpStorm\ArrayShape;
+use Symfony\Component\Config\Util\XmlUtils;
 
+/**
+ * Based on https://github.com/laminas/laminas-component-installer/blob/2.5.x/src/ComponentInstaller.php
+ *
+ * In order to have your xulieta extension auto configurable, you need to put in
+ * your composer.json the following keys, if applicable:
+ *
+ * - extra.xulieta.parser
+ * - extra.xulieta.validator
+ *
+ * The values should have the FQCN as the following example:
+ *
+ * <code class="lang-javascript">
+ * {
+ *   "extra": {
+ *     "xulieta": {
+ *       "parser": ["Malukenho\\QuoPrimumTempore\\JsonParser"],
+ *       "validator": ["Malukenho\\QuoPrimumTempore\\JsonValidator"]
+ *     }
+ *   }
+ * }
+ * </code>
+ */
 final class Register implements PluginInterface, EventSubscriberInterface
 {
     private Composer $composer;
@@ -26,24 +50,108 @@ final class Register implements PluginInterface, EventSubscriberInterface
     public function activate(Composer $composer, IOInterface $io)
     {
         $this->composer = $composer;
-        $this->io       = $io;
+        $this->io = $io;
     }
 
-    public static function scan(Event $composerEvent): void
+    public static function scan(PackageEvent $event): void
     {
-        $composerEvent->getIO()->write('Plugin is actually running');
+        if (! $event->isDevMode()) {
+            return;
+        }
 
-        // @TODO list all xulieta packages
-        // @TODO check for all xulieta packages, which one is configured with auto-enabling
-        // @TODO check in the config file if plugin is enabled
-        // @TODO provide a flag to turn off "auto-enabled installed packs"
+        $operation = $event->getOperation();
+        assert($operation instanceof InstallOperation);
+        $package = $operation->getPackage();
+        $name = $package->getName();
+
+        /** @var array<string,mixed> $packageExtra */
+        $packageExtra = $package->getExtra();
+        $extra = self::getExtraMetadata($packageExtra);
+
+//        if (empty($extra)) {
+        // Package does not define anything of interest; do nothing.
+//            return;
+//        }
+
+        $extra = [
+            "parser"    => ["Malukenho\\QuoPrimumTempore\\JsonParser"],
+            "validator" => ["Malukenho\\QuoPrimumTempore\\JsonValidator"],
+        ];
+
+        self::injectModuleIntoConfig($extra, $event->getIO(), $event->getComposer());
     }
 
-    #[ArrayShape([
-        PackageEvents::POST_PACKAGE_INSTALL   => "string",
-        PackageEvents::POST_PACKAGE_UPDATE    => "string",
-        PackageEvents::POST_PACKAGE_UNINSTALL => "string",
-    ])]
+    /**
+     * Retrieve the metadata from the "extra" section
+     *
+     * @param array<string,mixed> $extra
+     *
+     * @return array<string,mixed>
+     */
+    private static function getExtraMetadata(array $extra): array
+    {
+        $xulietaPluginConfiguration = [];
+
+        if (isset($extra['xulieta']) && is_array($extra['xulieta'])) {
+            /** @var array<string,mixed> $xulietaPluginConfiguration */
+            $xulietaPluginConfiguration = $extra['xulieta'];
+        }
+
+        return $xulietaPluginConfiguration;
+    }
+
+    private static function injectModuleIntoConfig(array $extra, IOInterface $io, Composer $composer): void
+    {
+        $rootDir = dirname($composer->getConfig()->getConfigSource()->getName());
+        $xulietaConfigFile = $rootDir . '/xulieta.xml';
+
+        if (! file_exists($xulietaConfigFile)) {
+            return;
+        }
+
+
+        // FIXME: Filter elements so it doesn't get repeated
+        $xml = XmlUtils::loadFile($xulietaConfigFile);
+        $root = $xml->documentElement;
+
+        $xml->preserveWhiteSpace = true;
+        $xml->formatOutput = true;
+
+        $parsers = $root->getElementsByTagName('parser');
+        $a = [];
+        foreach ($parsers->getIterator() as $registeredParsers) {
+            $a[] = $registeredParsers->textContent;
+        }
+
+        foreach ($extra['parser'] as $toBeRegistered) {
+            if (in_array($toBeRegistered, $a, true)) {
+                continue;
+            }
+
+            $parser = $xml->createElement('parser', $toBeRegistered);
+            $root->appendChild($parser);
+        }
+
+        $validators = $root->getElementsByTagName('validator');
+        $b = [];
+        foreach ($validators->getIterator() as $registeredValidators) {
+            $b[] = $registeredValidators->textContent;
+        }
+
+        foreach ($extra['validator'] as $toBeRegistered) {
+            if (in_array($toBeRegistered, $b, true)) {
+                continue;
+            }
+
+            $validator = $xml->createElement('validator', $toBeRegistered);
+            $root->appendChild($validator);
+        }
+
+        $xml->save($xulietaConfigFile);
+
+        $io->write('Updating file...');
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
